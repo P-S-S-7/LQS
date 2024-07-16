152a1a7a6040c6350bb460a3a2388941ad8f0c1e
P-S-S-7 <psinghskekhawat982@gmail.com>
2024-07-16 23:16:47 +0530

    Implemented ForgotPassword component with email input and reset functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Layout from './Layout';
 import Home from './components/Home';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
+import ForgotPassword from './components/ForgotPassword';
 
 const router = createBrowserRouter(
     createRoutesFromElements(
@@ -11,6 +12,7 @@ const router = createBrowserRouter(
         <Route path="" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
       </Route>
     )
 );
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,28 +3,25 @@ import { Link } from 'react-router-dom';
 import logo from '../assets/lnmiit.png';
 
 function Home() {
-
     return (
-        <section className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-purple-600 to-blue-600 text-white rounded-lg">
-            <div className="flex flex-col items-center justify-center h-full p-4 text-center">
-                <h3 className="text-5xl font-bold mb-6">
+        <section className="flex flex-col items-center justify-center bg-gradient-to-br from-purple-400 to-blue-400 text-white rounded-lg">
+            <div className="flex flex-col items-center justify-center p-4 text-center">
+                <h3 className="text-4xl md:text-5xl font-bold mb-4">
                     LNMIIT Quiz Scheduler
                 </h3>
-                <div className="bg-white rounded-full p-4 mb-6">
-                    <img
-                        src={logo}
-                        alt="LNMIIT Logo"
-                        className="rounded-full h-40 w-40 md:h-48 md:w-48 object-cover"
-                    />
-                </div>
+                <img
+                    src={logo}
+                    alt="LNMIIT Logo"
+                    className="rounded-full h-32 w-32 md:h-40 md:w-40 object-cover mb-4"
+                />
                 <Link
                     to="/sign-in"
-                    className="bg-white text-gray-800 hover:bg-gray-300 py-3 px-8 rounded-lg shadow-md transition duration-300 mt-8"
+                    className="bg-gradient-to-br from-purple-700 to-blue-500 text-gray-800 py-2 px-6 rounded-lg shadow-md transition duration-300 text-sm md:text-base"
                 >
-                    GET STARTED
+                   <strong>GET STARTED</strong> 
                 </Link>
             </div>
-            <footer className="absolute bottom-0 w-full text-center text-gray-300 py-4">
+            <footer className="text-center text-gray-300 py-2">
                 @created by - pss
             </footer>
         </section>
