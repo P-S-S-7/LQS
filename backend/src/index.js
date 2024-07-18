602b193d6fdb860a2ab21deac88cd1ef20f4f8cc
P-S-S-7 <psinghskekhawat982@gmail.com>
2024-07-18 18:40:55 +0530

    Custom api response and error handling - utilities

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,15 @@
-import mongoose from "mongoose";
+import {app} from "./app.js";
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-import { Student } from "./models/student.model.js";
-import { Faculty } from "./models/faculty.model.js";
 
-connectDB();
+dotenv.config({path: "./.env"});
+
+connectDB()
+.then (() => {
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is running at port:${process.env.PORT}`);
+  })
+})
+.catch((err) => {
+    console.error("MongoDB connection error: ", err);
+})
---
+++ backend/src/utils/ApiResponse.js
@@ -0,0 +1,10 @@
+const ApiResponse = {
+    constructor(statusCode, data, message = "Succcess") {
+        this.statusCode = statusCode;
+        this.data = data;
+        this.message = message;
+        this.success = statusCode < 400;
+    }
+}
+
+export { ApiResponse };
