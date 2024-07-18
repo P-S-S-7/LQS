e657e8e86a35430d1f51aee375e162126c57c544
P-S-S-7 <psinghskekhawat982@gmail.com>
2024-07-18 18:43:36 +0530

    Added middleware for CORS, body parsing, and cookies

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,20 @@
 import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const app = express();
+
+app.use(cors(
+    {
+        origin: process.env.CLIENT_URL,
+        credentials: true,
+    }
+));
+
+app.use(express.json({ limit: "16kb" })); // our app now accepts JSON data in the body of the request - body-parser is now included in express (Middleware function)
+
+app.use(express.urlencoded({ extended: true, limit: "16kb" })); // our app now handles URL encoded data - body-parser is now included in express (Middleware function)
+
+app.use(cookieParser()); // our app now uses cookie-parser to parse cookies in the request headers
 
 export {app};
