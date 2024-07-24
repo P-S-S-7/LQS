777a82c5e47e6f15ae1b8beab1da747c8edbf371
P-S-S-7 <psinghshekhawat982@gmail.com>
2024-07-24 18:45:10 +0530

    CORS error

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,12 +4,7 @@ import cookieParser from 'cookie-parser';
 
 const app = express();
 
-app.use(cors(
-    {
-        origin: process.env.CLIENT_URL ,
-        credentials: true,
-    }
-));
+app.use(cors());
 
 
 app.use(express.json({ limit: "16kb" })); // body-parser (Middleware to parse JSON data)
