961d680ab5a294b38f722218dda151c4f6068129
P-S-S-7 <psinghshekhawat982@gmail.com>
2024-07-24 21:07:52 +0530

    render backend

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,4 @@
-const REACT_APP_API_URI = "https://lqs-server.vercel.app/api/v1";
+const REACT_APP_API_URI = "https://lqs.onrender.com/api/v1";
 const studentEmailRegex = /^[1-9][0-9][a-zA-Z]{3}[0-9]{2}[1-9]@lnmiit\.ac\.in$/;
 const facultyEmailRegex = /^[a-zA-Z]{1}[a-zA-Z0-9]{1,}@lnmiit\.ac\.in$/; 
 const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
